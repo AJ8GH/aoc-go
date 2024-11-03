1b145d657a9e8fa5244bc3efdb4348b2c2807db1
Adam Jonas <adamjonas25@gmail.com>
2024-11-03 22:13:17 +0000

    Add files for day 1

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Solving Advent of code using Go and TDD
 
+This project uses [my Advent of Code CLI utility](https://github.com/AJ8GH/aoc-cli-2.0)
+to generate skeleton code and tests, input files, and README files for each day.
+
 ## Dependencies
 
 * go 1.23
---
+++ year-15/day-1/day1_test.go
@@ -0,0 +1,49 @@
+package day1
+
+import (
+	"testing"
+
+	"github.com/aj8gh/aocgo/util"
+)
+
+var testCases = []struct {
+	name    string
+	subject func([]string) int
+	input   []string
+	want    int
+}{
+	{
+		name:    "Day 1 Level 1 Example",
+		subject: Level1,
+		input:   util.ReadExample(),
+		want:    0,
+	},
+	{
+		name:    "Day 1 Level 1",
+		subject: Level1,
+		input:   util.ReadInput(),
+		want:    74,
+	},
+	{
+		name:    "Day 1 Level 2 Example",
+		subject: Level2,
+		input:   util.ReadExample(),
+		want:    0,
+	},
+	{
+		name:    "Day 1 Level 2",
+		subject: Level2,
+		input:   util.ReadInput(),
+		want:    1795,
+	},
+}
+
+func TestDay1(t *testing.T) {
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.subject(tt.input)
+			util.Assert(t, got, tt.want)
+		})
+	}
+}
+

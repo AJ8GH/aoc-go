d48bce1a9dbfde77aece6af099ca87dbd9b0a778
Adam Jonas <adamjonas25@gmail.com>
2024-12-13 23:01:16 +0000

    Solve y24 d4 l1

--- year-24/day-4/day4.go
+++ year-24/day-4/day4.go
@@ -1,9 +1,118 @@
 package day4
 
+import (
+	"unicode/utf8"
+)
+
+const (
+	x = "X"
+	m = "M"
+	a = "A"
+	s = "S"
+
+	north     = "n"
+	northEast = "ne"
+	east      = "e"
+	southEast = "se"
+	south     = "s"
+	southWest = "sw"
+	west      = "w"
+	northWest = "nw"
+)
+
+var directions = []string{
+	north,
+	northEast,
+	east,
+	southEast,
+	south,
+	southWest,
+	west,
+	northWest,
+}
+
 func Level1(input []string) int {
-	return 0
+	xLocs := [][]int{}
+	for i, line := range input {
+		for j, char := range line {
+			if string(char) == x {
+				xLocs = append(xLocs, []int{i, j})
+			}
+		}
+	}
+	return countXmas(xLocs, input)
 }
 
 func Level2(input []string) int {
 	return 0
+}
+
+func countXmas(xLocs [][]int, input []string) int {
+	xmasCount := 0
+	for _, loc := range xLocs {
+		for _, dir := range directions {
+			xmasCount += countForLoc(loc, input, x, dir)
+		}
+	}
+	return xmasCount
+}
+
+func countForLoc(loc []int, input []string, letter string, direction string) int {
+	nextLoc := nextLoc(loc, direction)
+	nextLetter := getLetter(nextLoc, input)
+	if nextLetter == next(letter) {
+		if nextLetter == s {
+			return 1
+		}
+		return countForLoc(nextLoc, input, nextLetter, direction)
+	}
+	return 0
+}
+
+func nextLoc(loc []int, direction string) []int {
+	switch direction {
+	case north:
+		return []int{loc[0] + 1, loc[1]}
+	case northEast:
+		return []int{loc[0] + 1, loc[1] + 1}
+	case east:
+		return []int{loc[0], loc[1] + 1}
+	case southEast:
+		return []int{loc[0] - 1, loc[1] + 1}
+	case south:
+		return []int{loc[0] - 1, loc[1]}
+	case southWest:
+		return []int{loc[0] - 1, loc[1] - 1}
+	case west:
+		return []int{loc[0], loc[1] - 1}
+	case northWest:
+		return []int{loc[0] + 1, loc[1] - 1}
+	default:
+		panic("Bad direction")
+	}
+}
+
+func next(r string) string {
+	switch r {
+	case x:
+		return m
+	case m:
+		return a
+	case a:
+		return s
+	default:
+		return ""
+	}
+}
+
+func getLetter(loc []int, input []string) string {
+	i, j := loc[0], loc[1]
+	if i >= 0 && i < len(input) {
+		line := input[i]
+		if j >= 0 && j < utf8.RuneCountInString(line) {
+			return string(line[j])
+		}
+	}
+
+	return ""
 }
--- year-24/day-4/day4_test.go
+++ year-24/day-4/day4_test.go
@@ -16,19 +16,19 @@ var testCases = []struct {
 		name:    "Day 4 Level 1 Example",
 		subject: Level1,
 		input:   util.ReadExample(),
-		want:    0,
+		want:    18,
 	},
 	{
 		name:    "Day 4 Level 1",
 		subject: Level1,
 		input:   util.ReadInput(),
-		want:    0,
+		want:    2685,
 	},
 	{
 		name:    "Day 4 Level 2 Example",
 		subject: Level2,
 		input:   util.ReadExample(),
-		want:    0,
+		want:    9,
 	},
 	{
 		name:    "Day 4 Level 2",
@@ -47,3 +47,36 @@ func TestDay4(t *testing.T) {
 	}
 }
 
+/*
+
+  | a  b  c  d  e  f  g  h  i  j
+--+------------------------------
+0 | .  .  .  .  X  X  M  A  S  .
+1 | .  S  A  M  X  M  S  .  .  .
+2 | .  .  .  S  .  .  A  .  .  .
+3 | .  .  A  .  A  .  M  S  .  X
+4 | X  M  A  S  A  M  X  .  M  M
+5 | X  .  .  .  .  .  X  A  .  A
+6 | S  .  S  .  S  .  S  .  S  S
+7 | .  A  .  A  .  A  .  A  .  A
+8 | .  .  M  .  M  .  M  .  M  M
+9 | .  X  .  X  .  X  M  A  S  X
+
+
+  | a  b  c  d  e  f  g  h  i  j
+--+------------------------------
+0 | M  M  M  S  X  X  M  A  S  M
+1 | M  S  A  M  X  M  S  M  S  A
+2 | A  M  X  S  X  M  A  A  M  M
+3 | M  S  A  M  A  S  M  S  M  X
+4 | X  M  A  S  A  M  X  A  M  M
+5 | X  X  A  M  M  X  X  A  M  A
+6 | S  M  S  M  S  A  S  X  S  S
+7 | S  A  X  A  M  A  S  A  A  A
+8 | M  A  M  M  M  X  M  M  M  M
+9 | M  X  M  X  A  X  M  A  S  X
+
+
+
+
+*/
